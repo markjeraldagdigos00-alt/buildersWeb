80c1988f26bd23e81739de93a66e925a99ef6369
markjeraldagdigos00-alt <markjeraldagdigos00@gmail.com>
2026-08-19 08:57:12 +0800

    Update allportals.js

--- allportals.js
+++ allportals.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 5432;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
